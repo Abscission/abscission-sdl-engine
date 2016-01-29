6a5fe4a66e14c8b9ff52c4c797fce6c8e8f56823
Xwing669 <dan.alexander@slingshot.co.nz>
2016-01-30 11:50:12 +1300

    started the main menu

---
+++ src/gs_menu.cpp
@@ -0,0 +1,25 @@
+#include "gs_menu.h"
+#include "renderer.h"
+#include "con_vars.h"
+
+void MenuState::init()
+{
+	//TODO(Xwing); Start main menu music
+}
+
+void MenuState::shutdown()
+{
+}
+
+void MenuState::update()
+{
+}
+
+void MenuState::draw()
+{
+	g_renderer->draw_text("Play", 50/*g_cvars->resolution_x*/, 250, 17.f, SDL_Color{ 255, 255, 255, 255});
+	g_renderer->draw_text("Options", 50/*g_cvars->resolution_x*/, 300, 17.f, SDL_Color{ 255, 255, 255, 255 });
+	g_renderer->draw_text("Credits", 50/*g_cvars->resolution_x*/, 350, 17.f, SDL_Color{ 255, 255, 255, 255 });
+	g_renderer->draw_text("Quit", 50/*g_cvars->resolution_x*/, 400, 17.f, SDL_Color{ 255, 255, 255, 255 });
+
+}
