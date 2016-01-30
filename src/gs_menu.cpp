185a2cc3127ad83912dd2760d8f68ff4b40f6c34
Xwing669 <dan.alexander@slingshot.co.nz>
2016-01-31 01:31:29 +1300

    menus better damn work

--- src/gs_menu.cpp
+++ src/gs_menu.cpp
@@ -1,30 +1,27 @@
 #include "gs_menu.h"
 #include "gs_cardgame.h"
 #include "gs_options.h"
-
 #include "renderer.h"
 #include "audio.h"
 #include "input.h"
 #include "console.h"
 #include "abscission_math.h"
 
-void MenuState::init()
-{
+void MenuState::init() {
 	//TODO(Xwing); Start main menu music
 	g_sound_manager.play_file_loop("assets/Dream World.ogg");
 }
 
-void MenuState::shutdown()
-{
+void MenuState::shutdown() {
 }
 
-void MenuState::update()
-{
+void MenuState::update() {
 	
 }
 
-void MenuState::draw()
-{
+void MenuState::draw() {
+	
+	g_renderer->draw_rect(0, 0, g_renderer->width, g_renderer->height, SDL_Color{ 0, 208, 42, 255 });
 	SDL_Rect play_hitbox = g_renderer->draw_text("Play", g_renderer->width / 20, int(0.75f * g_renderer->height), 20, SDL_Color{ 255, 255, 255, 255});
 	SDL_Rect options_hitbox = g_renderer->draw_text("Options", g_renderer->width / 20, int(0.8f * g_renderer->height), 20, SDL_Color{ 255, 255, 255, 255 });
 	SDL_Rect credits_hitbox = g_renderer->draw_text("Credits", g_renderer->width / 20, int(0.85f * g_renderer->height), 20, SDL_Color{ 255, 255, 255, 255 });
@@ -32,8 +29,8 @@ void MenuState::draw()
 
 	if (g_input_manager.click_thisframe) {
 		if (is_mouse_over(play_hitbox)) {
-			//play
-
+			CardState* c = new CardState;
+			GameState::create_game_state(c);
 		}
 		if (is_mouse_over(options_hitbox)) {
 			OptionState* o = new OptionState;
