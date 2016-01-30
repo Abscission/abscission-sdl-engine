72bf91f8dcf713f8f75beee4d8442fa16a822e2f
AdamVanOijen <adamvanoijen1997@gmail.com>
2016-01-30 23:38:27 +1300

    Battle state, linux support

---
+++ src/include/gs_battle.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "card.h"
+#include "renderer.h"
+#include "game_state.h"
+#include "types.h"
+
+class BattleState : public GameState {
+	Card* card_a;
+	Card* card_b;
+
+	Image monster_a;
+	Image monster_b;
+
+	v2 monster_a_position;
+	v2 monster_b_position;
+
+
+public:
+	BattleState(Card* a, Card* b);
+
+	virtual void init() final;
+	virtual void shutdown() final;
+
+	virtual void update() final;
+	virtual void draw() final;
+};
